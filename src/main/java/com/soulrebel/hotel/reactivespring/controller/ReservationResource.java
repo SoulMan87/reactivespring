875033d3002045f2a890e0abbf695295255a7b5d
Jonathan <jonathanhinestroza87@gmail.com>
2020-08-26 21:22:26 -0500

    updatePrice and DeleteReservation methods

--- src/main/java/com/soulrebel/hotel/reactivespring/controller/ReservationResource.java
+++ src/main/java/com/soulrebel/hotel/reactivespring/controller/ReservationResource.java
@@ -11,14 +11,28 @@ import reactor.core.publisher.Mono;
 public class ReservationResource {
 
     public static final String ROOM_V_1_RESERVATION = "/room/v1/reservation";
+
     @GetMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> getReservationById(@PathVariable String roomId){
+    public Mono<String> getReservationById(@PathVariable String roomId) {
         //reservationService.getReservation(roomId);
         return Mono.just("{}");
     }
+
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String>createReservation(@RequestBody Mono<Reservation> reservation){
+    public Mono<String> createReservation(@RequestBody Mono<Reservation> reservation) {
         return Mono.just("{}");
+    }
+
+    @PutMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> updatePrice(@PathVariable String roomId,
+                                    @RequestBody Mono<Reservation> reservation) {
+        return Mono.just("{}");
+    }
+    @DeleteMapping(path = "{roomId}")
+    public Mono<Boolean> deleteReservation(@PathVariable String roomId){
+
+        return Mono.just(true);
     }
 }
